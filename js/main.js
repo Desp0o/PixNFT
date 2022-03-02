7274f0b3854355e0ae99eb960a8dac87e1668d0d
Desp0o <tornike.despotashvili@gmail.com>
2022-03-02 14:20:23 +0400

    back
    back

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ let noscroll = document.querySelector('body');
 headerburger.onclick = function(){
     headerburger.classList.toggle('active');
     menu.classList.toggle('active');
-    
+    noscroll.classList.toggle('no-scroll');
 } 
 
 menu.onclick = function(){ 
     menu.classList.toggle('active');
-    
+   
 } 
 
