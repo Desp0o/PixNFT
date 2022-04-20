a3e471143ff34cd4730d31d0626d2f12dcc62a5d
Tornike Despotashvili <95018466+Desp0o@users.noreply.github.com>
2022-04-20 21:55:03 +0400

    dark mode

--- js/main.js
+++ js/main.js
@@ -14,3 +14,38 @@ menu.onclick = function(){
     headerburger.classList.toggle('active');
 } 
 
+$(".mode-icon").click(function(){
+    
+    document.querySelector("body").classList.toggle('darkclass');  
+    document.querySelector(".fixed-menu").classList.toggle("fixed-menu-dark");
+    document.querySelector(".container-blue-bg").classList.toggle('darkclass');
+    document.querySelector(".roadmap-content").classList.toggle("darkclass");
+    document.querySelector(".container-rarity").classList.toggle("darkclass");
+    document.querySelector(".creator-content").classList.toggle("darkclass");
+    document.querySelector(".FAQ-content").classList.toggle("darkclass");
+    document.querySelector(".Get-your-nft").classList.toggle("darkclass");
+    document.querySelector("#opneseayellow").classList.toggle("opneseapink");
+    document.querySelector(".get-yours-now-btn").classList.toggle("get-yours-now-btn-dark");
+    document.querySelector(".creator-btn-link").classList.toggle("creator-btn-link-dark");
+    document.querySelector(".Get-your-nft-left-btn-link").classList.toggle("Get-your-nft-left-btn-link-dark");
+    document.querySelector("#opensea").classList.toggle("opensea-dark");
+
+    for(i=0; i<document.querySelectorAll(".left-side-list-items").length; i++){
+        document.querySelectorAll(".left-side-list-items")[i].classList.toggle("left-side-list-items-dark");
+    }
+
+    for(i=0; i<document.querySelectorAll(".footer-links").length; i++){
+    document.querySelectorAll(".footer-links")[i].classList.toggle("footer-links-dark");
+    }
+
+    for(i=0; i<document.querySelectorAll(".question").length; i++){
+        document.querySelectorAll(".question")[i].classList.toggle("question-dark");
+    }
+
+    for(i=0; i<document.querySelectorAll(".roadmap-column-list").length; i++){
+        document.querySelectorAll(".roadmap-column-list")[i].classList.toggle('roadmap-column-list-dark');
+    }
+    
+    
+    
+});
